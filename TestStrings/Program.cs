a4cf50b01416c02c9a12a2c6e7b6b745b80ae8d0
kamel-rush <kamel@grandcircus.co>
2016-11-09 23:16:28 -0500

    Initial

---
+++ TestStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+
+            //Console.WriteLine(input.PadRight(10, '*'));
+
+           
+
+           // string[] ar = input.Split(',', '.');
+
+            //Console.WriteLine(ar[0]);
+            //Console.WriteLine(ar[1]);
+
+            Console.WriteLine(input.Substring(5));
+            Console.WriteLine(input.Length);
+
+            
+        }
+    }
+}
